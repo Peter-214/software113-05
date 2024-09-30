cbfc28e4eda3e38918892b809dea2575b7b405b0
Leon <137614370+Arctic0725@users.noreply.github.com>
2024-09-30 10:42:03 +0800

    Test the file for branch error

---
+++ test.h
@@ -0,0 +1,18 @@
+#ifndef test.h
+#define test.h
+#include <iostream>
+
+using namespace std;
+class Test
+{
+private:
+    /* data */
+public:
+    void Test_1(){
+       cout << "Test_1" << endl;
+    }
+    Test(/* args */);
+    ~Test();
+};
+
+#endif
